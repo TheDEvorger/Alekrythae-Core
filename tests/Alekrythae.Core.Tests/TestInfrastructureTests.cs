c9b64810d5e6de6865ccb6de1bdbdc4c00174493
Ayberk Erdem <thedevorger.alekrythae.dev@gmail.com>
2026-09-20 20:35:24 +0300

    Add initial unit tests for TestInfrastructure

---
+++ tests/Alekrythae.Core.Tests/TestInfrastructureTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alekrythae.Core.Tests;
+
+[TestClass]
+public sealed class TestInfrastructureTests
+{
+    [TestMethod]
+    public void TestRunner_ShouldRunSuccessfully()
+    {
+        Assert.IsTrue(true);
+    }
+}
